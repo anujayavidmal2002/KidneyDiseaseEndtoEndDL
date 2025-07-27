e63d5d00e151ad46c2607599ab433225c757fe0e
anujayavidmal2002 <vidmalssaa.22@uom.lk>
2025-07-27 09:56:21 +0530

    data ingestion addded

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-from src.cnnClassifier import logger
+import sys
+import os
+
+# Add src directory to Python path
+sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("Welcome to our custom log")
 
 
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
